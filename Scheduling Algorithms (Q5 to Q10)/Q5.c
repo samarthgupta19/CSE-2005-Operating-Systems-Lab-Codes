06044379ee29b5df49d87afcfb26b64582a2e694
Samarth Gupta <53513648+samarthgupta19@users.noreply.github.com>
2021-06-07 15:32:01 +0530

    Added All Codes

--- Scheduling Algorithms (Q5 to Q10)/Q5.c
+++ Scheduling Algorithms (Q5 to Q10)/Q5.c
@@ -1,6 +1,8 @@
-    #include <stdio.h>
-     #include <stdlib.h>
-     #define MAX 100
+// 19BDS0042 SAMARTH GUPTA
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
      typedef struct
          { int pid;
            int burst_time;
